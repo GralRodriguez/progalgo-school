1eda737b28edf9630869fd2e524bde812512892f
Héctor Giovanni Rodríguez Ramos <gral.rodriguezr@gmail.com>
2022-09-27 21:13:48 -0500

    GralRodriguez: Martes 27, Septiembre 2022: Primeras migraciones de tablas integradas

---
+++ database/migrations/2022_07_27_042452_create_school_grade_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school_grade', function (Blueprint $table) {
+            $table->id();
+            $table->string('school_grade_name');
+            $table->string('school_grade_description');
+            $table->string('school_grade_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school_grade');
+    }
+};
